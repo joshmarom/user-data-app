5ff1c931f3904acddd5de2dd09c4a819e48fce53
Josh Marom <josh@elementor.com>
2021-01-05 19:52:46 +0200

    Add react hook form package

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useForm } from 'react-hook-form';
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+        <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
+        <input type="text" placeholder="Last name" name="Last name" ref={register({required: true, maxLength: 100})} />
+        <input type="text" placeholder="Password" name="Password" ref={register({required: true, maxLength: 50, pattern: /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/i})} />
+
+        <input type="submit" />
+      </form>
   );
 }
-
-export default App;
